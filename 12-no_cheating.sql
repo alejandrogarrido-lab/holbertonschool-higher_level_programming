144da60f998476c9234623184eb808ef9e88928a
alejandrogarrido-lab <alejandro_garrido@anahuacmayab.edu.mx>
2026-02-04 19:07:24 +0100

    Move all SQL files to root directory

---
+++ 12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Update score of Bob to 10 
+-- Tihs script updates the score of Bob using only the name field
+UPDATE second_table SET score = 10 WHERE name = "Bob";
---
+++ 7-cities.sql
@@ -0,0 +1,12 @@
+-- CREATE hbtn_0d_usa and cities table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+       id INT NOT NULL AUTO_INCREMENT,
+       UNIQUE (id),
+       PRIMARY KEY (id),
+       state_id INT NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id),
+       name VARCHAR(256) NOT NULL
+);
