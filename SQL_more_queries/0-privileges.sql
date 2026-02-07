cca009fac3e0316a77cd521635042290846d93a1
Alejandro Garrido <alejandro_garrido@anahuacmayab.edu.mx>
2026-02-07 17:40:29 +0000

    Fix 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
